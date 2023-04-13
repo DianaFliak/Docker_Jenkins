65ac5a64bc917dfe0cf8298d6e82e14f2d39273a
Diana Fliak <90823842+DianaFliak@users.noreply.github.com>
2023-04-14 00:48:40 +0300

    Delete apteka directory

--- apteka/assets/js/script.js
+++
@@ -1,6 +0,0 @@
-function hello() {
-	alert("hello");
-}
-function resizeIframe(obj) {
-	    obj.style.height = document.documentElement.scrollHeight - document.getElementById('header').scrollHeight - document.getElementById('koz').scrollHeight - document.getElementById('footer').scrollHeight - 71 + 'px';
-}
